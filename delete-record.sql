7855a8f8a0bc293c366906b37e1698213a664657
Ivan Madrazo <ivanm.sci81@gmail.com>
2023-02-05 11:08:13 -0600

    clase 16

---
+++ delete-record.sql
@@ -0,0 +1,14 @@
+USE metro_cdmx;
+DELETE FROM `stations`
+WHERE id = 163;
+DELETE FROM `stations`
+WHERE id = 164;
+ELETE FROM `stations`
+WHERE id = 165;
+DELETE FROM `stations`
+WHERE id = 166;
+
+--tambien se pueden borrar o filtrar datos por los nombre, no solo por el id:
+
+-- DELETE FROM `stations`
+-- WHERE name = "Benito Cardenas"
